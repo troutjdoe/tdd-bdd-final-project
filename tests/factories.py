37870f3102c10fda5f24ca655417ac3f9ef1acb3
Your Name <you@example.com>
2024-09-23 03:44:10 -0400

    test cases for model

--- tests/factories.py
+++ tests/factories.py
@@ -50,7 +50,7 @@ class ProductFactory(factory.Factory):
     
     description = factory.Faker("text")
     price = FuzzyDecimal(0.5, 2000.0, 2)
-    available = FuzzyChoice(choices=[Trues, False])
+    available = FuzzyChoice(choices=[True, False])
     category = FuzzyChoice(
         choices=[
             Category.UNKNOWN,
